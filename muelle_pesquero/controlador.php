e1c75e3217be9cf3163088eccf3cec9313132c93
carlos1617 <cepm1315475358@gmail.com>
2023-09-23 21:47:06 -0500

    muelle

---
+++ muelle_pesquero/controlador.php
@@ -0,0 +1,20 @@
+<?php
+if (!empty($_POST["send3"])){
+    if (empty($_POST["user"]) and empty ($_POST["contra"])){
+        echo "Los campos están vacíos";
+    }else{
+       $user=$_POST["user"];
+       $contra=$_POST["contra"];
+       $sql=$conex->query(" select * from usuarios where user='$user' and contra='$contra' ");
+       if ($datos=$sql->fetch_object()){
+        ?>
+        <script type="text/javascript"> 
+            window.location="mantenimientobarco.php"; 
+        </script> 
+        <?php
+       }else{
+        echo "Usuario no existente";
+       }
+    }
+}
+?>
